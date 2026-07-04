6d86f75e0641a611c1d083db265a48446837b7bf
albatross101 <Andrew.tao.sunshine.111@gmail.com>
2026-07-04 10:29:21 -0400

    Added analysis and comparison for hand-wing index and order counts for AVONET and AVOTREX

--- AVONET_analysis.py
+++ AVONET_analysis.py
@@ -19,6 +19,17 @@ culmen = df["Beak.Length_Culmen"]
 with open("Culmen_length_comparison.txt", "a") as file:
     file.write("Average Culmen Length of Living Birds: " + str(culmen.mean()) + " mm" + "\n")
 
+#HWI
+hand_wing_index = df["Hand-Wing.Index"]
+with open("HWI_comparison.txt", "a") as file:
+    file.write("Average Hand Wing Index of Living Birds: " + str(hand_wing_index.mean()) + "\n")
+
+#Order counts
+order_counts = df["Order1"].value_counts()
+with open("Order_counts_AVONET.txt", "a") as file:
+    file.write("Order counts of Living Birds: " + str(order_counts) + "\n")
+
+
 
 
 
--- AVOTREX_analysis.py
+++ AVOTREX_analysis.py
@@ -14,7 +14,17 @@ mass = df["body_mass"]
 with open("Mass_comparison.txt", "a") as file:
     file.write("Average Mass of Extinct Birds: " + str(mass.mean()) + " g" + "\n")
 
-#Hand-Wing Index
+#Culmen length
 culmen_length = df["beak_length_culmen"]
 with open("Culmen_length_comparison.txt", "a") as file:
     file.write("Average Culmen Length of Extinct Birds: " + str(culmen_length.mean()) + " mm" + "\n")
+
+#Hand-Wing Index
+hand_wing_index = (df["kipps_distance"]/df["wing_length"] * 100)
+with open("HWI_comparison.txt", "a") as file:
+    file.write("Average Hand-Wing Index of Extinct Birds: " + str(hand_wing_index.mean()) + "\n")
+
+#Order counts
+order_counts = df["order"].value_counts()
+with open("Order_counts_AVOTREX.txt", "a") as file:
+    file.write("Order counts of Extinct Birds: " + str(order_counts) + "\n")
