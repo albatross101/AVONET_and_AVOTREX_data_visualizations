838f718d2341edc6053ff5c094878b374f9d6dd8
albatross101 <Andrew.tao.sunshine.111@gmail.com>
2026-07-06 08:49:43 -0400

    Added visualization for flight ability and endemicity of extinct birds in AVOTREX_visualization.py

--- AVOTREX_visualization.py
+++ AVOTREX_visualization.py
@@ -6,11 +6,6 @@ df = pd.read_csv("data/AVOTREX.csv")
 #Order counts and proportions
 order_counts = df["order"].value_counts()
 order_proportions_AVOTREX = df["order"].value_counts(normalize=True)
-
-#Flight ability counts
-flight_ability_counts = df["flight_ability"].value_counts()
-print(flight_ability_counts)
-
 plt.bar(order_proportions_AVOTREX.index, order_proportions_AVOTREX.values)
 plt.xlabel("Order")
 plt.ylabel("Proportion")
@@ -19,9 +14,21 @@ plt.xticks(rotation=90)
 plt.tight_layout()
 plt.show()
 
-plt.bar(flight_ability_counts.index.astype(str), flight_ability_counts.values, width=0.4)
+#Flight ability counts
+flight_ability_counts = df["flight_ability"].value_counts()
+plt.bar(flight_ability_counts.index.astype(str), flight_ability_counts.values, width=0.4, label=("0 = Flightless, 0.5 = Weak flyer, 1 = Flighted"))
 plt.xlabel("Flight Ability")
 plt.ylabel("Number of Species")
 plt.title("Flight Ability of Extinct Birds")
+plt.tight_layout()
+plt.legend()
+plt.show()
+
+#Endemicity
+endemicity_counts = df["island_endemicity"].value_counts()
+plt.bar(endemicity_counts.index, endemicity_counts.values)
+plt.xlabel("Endemicity")
+plt.ylabel("Number of Species")
+plt.title("Endemicity of Extinct Birds")
 plt.tight_layout()
 plt.show()
